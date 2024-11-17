8ec37a77c90e92f7db165a9ebe2b6f28614eac93
MostafaBastawyy <mostafa@petsoul.com>
2024-11-17 23:07:03 +0200

    comment all services screen

--- lib/view/home_view/nav_bar_view.dart
+++ lib/view/home_view/nav_bar_view.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:shiplan_service/view/home_view/addresess_view.dart';
 import 'package:shiplan_service/view/home_view/home_view.dart';
-import 'package:shiplan_service/view/home_view/mids_list_view.dart';
 import 'package:shiplan_service/view/profile_view/main_profile_view.dart';
-import 'package:shiplan_service/view/profile_view/profile_view.dart';
 
 import '../../constants.dart';
-import 'Favorite_view.dart';
 
 class NavBarView extends StatefulWidget {
   const NavBarView({super.key});
@@ -15,13 +10,15 @@ class NavBarView extends StatefulWidget {
   @override
   State<NavBarView> createState() => _NavBarViewState();
 }
+
 int _selectedIndex = 0;
- List<Widget> _pages = [
-  HomeView(),MidsListScreen(),MainProfileView(),
+List<Widget> _pages = [
+  HomeView(),
+  // MidsListScreen(),
+  MainProfileView(),
 ];
+
 class _NavBarViewState extends State<NavBarView> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,13 +29,20 @@ class _NavBarViewState extends State<NavBarView> {
           onTap: _onItemTapped,
           selectedItemColor: mainColor,
           unselectedItemColor: Colors.black,
-          type : BottomNavigationBarType.fixed,
+          type: BottomNavigationBarType.fixed,
           items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home_outlined,size: 30,),label: 'الرئيسية'),
-            BottomNavigationBarItem(icon: Icon(Icons.list,size: 30),label: 'جميع الخدمات'),
-            BottomNavigationBarItem(icon: Icon(Icons.person_outline_outlined,size: 30),label: 'الملف الشخصي'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home_outlined,
+                  size: 30,
+                ),
+                label: 'الرئيسية'),
+            // BottomNavigationBarItem(
+            //     icon: Icon(Icons.list, size: 30), label: 'جميع الخدمات'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline_outlined, size: 30),
+                label: 'الملف الشخصي'),
           ],
-
         ),
       ),
       body: Center(
@@ -46,6 +50,7 @@ class _NavBarViewState extends State<NavBarView> {
       ),
     );
   }
+
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
